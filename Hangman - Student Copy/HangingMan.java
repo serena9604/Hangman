11b087f9213aaec76d0628446e4b8f632ae7dd7c
Serena Bi <56049445+serena9604@users.noreply.github.com>
2020-02-22 20:40:41 -0800

    Add files via upload

--- Hangman - Student Copy/HangingMan.java
+++ Hangman - Student Copy/HangingMan.java
@@ -1,6 +1,7 @@
 public class HangingMan {
 
     private int numWrongGuesses;
+    public int lives;
     String [] h = new String[] {""};
     String[] HangmanImage = {"+----+\n"+
             "|\n"+
@@ -89,29 +90,44 @@ public class HangingMan {
             "|   / \\\n"+
             "/\\\n",
         };
+        lives = 6;
     }
 
-    public static void printString(String[] h) {
+    public static void printString(String[] h, int r) {
         String breakApart = "";
-        for (int r = 0; r < h.length; r ++) {
-            System.out.print(h[r] + "Mike" + h.length);
-
+        /* for (int r = start; r <= 1; r ++) {
+        System.out.print(h[r]);
         }
+         */
+        System.out.print(h[r] + "Mike");
     }
 
     public void show () {
         //picks which picture to show
-        printString(h);
+        if (lives == 6) {
+            printString(h, 0);
+        } else if (lives == 5) {
+            printString(h, 1);
+        } else if (lives == 4) {
+            printString(h, 2);
+        } else if (lives == 3) {
+            printString(h, 3);
+        } else if (lives == 2) {
+            printString(h, 4);
+        } else if (lives == 1) {
+            printString(h, 5);
+        } 
     }
 
     public void dieSomeMore() {
         //add hangman part (eg leg, arm) to man hanging from tree
-
+        lives -= 1;
     }
 
     public boolean isntDead(){
         //check to see if hangman is dead or not
         //checks whether all his parts are hanging from tree or not
-        return true;
+        if (lives == 0) return true;
+        else return false;
     }
 }
--- Hangman - Student Copy/Puzzle.java
+++ Hangman - Student Copy/Puzzle.java
@@ -6,29 +6,89 @@ import java.io.FileNotFoundException;
 public class Puzzle
 {
     //display of letters and blanks
-    String s;
+    String s = "";
+    String guessedLetters = "";
+    String tempWord ="";
+    ArrayList<String> guess = new ArrayList<String>();
+    ArrayList<String> puzzle = new ArrayList<String>(); 
+    ArrayList<String> wordList = new ArrayList<String>();
+    ArrayList<String> words= new ArrayList<String>();
+
     public Puzzle() {
         //picks combination of words and blanks to show
+        words = new ArrayList<String>();
+
+        try {
+            File file = new File("words.txt");
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNext()) {
+                String tempWord = scanner.next().toUpperCase();
+                words.add(tempWord);
+            }
+            scanner.close();
+
+            //ONCE THIS LINE OF CODE IS REACHED, YOUR words ArrayList
+            //CONTAINS ALL THE WORDS IN words.txt
+            tempWord = words.get((int)(Math.random()*words.size()));
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < puzzle.size(); i ++) {
+            for (int j = 0; j < guess.size(); j ++) {
+                if (guess.get(i).equals(puzzle.get(j))) puzzle.set(j, guess.get(i)); 
+                else puzzle.set(j, "_");
+            }
+        }
+
+        for (int k = 0; k < tempWord.length(); k ++) {
+            puzzle.set(k, tempWord.substring(k, k + 1));
+        }
+    }
+
+    public static void printString(ArrayList<String> puzzle) {
+        for (int i = 0; i < puzzle.size(); i ++) {
+            System.out.println(puzzle.get(i));
+        }
     }
 
     public void show () {
         //shows combination of words and blanks chosen in Puzzle
+        printString(puzzle);
+
     }
 
     public boolean isUnsolved() {
-        //checks too see if you have guessed all the letters in the puzzle or not
-        return true;
+        //checks to see if you have guessed all the letters in the puzzle or not
+        int sum = 0;
+        for (int i = 0; i < guess.size(); i ++) {
+            for (int j = 0; j < puzzle.size(); j ++) {
+                if (puzzle.get(j) == guess.get(i)) {
+                    sum ++;
+                }
+            }
+        }
+        if (sum == puzzle.size()) return true;
+        else return false;
     }
 
     public boolean makeGuess(String g) {
         //makes the guess
         //adds to collection of guessed letters: can make it ArrayList OR String
         //when code puzzle, print underscore if letter not in list of guesses and print letter letter if in list of guesses
-        return false;
+        guess.add(g);
+        if (puzzle.contains(g)) {
+            return true;
+        }
+        else return false;
     }
 
     public String getWord() {
         //gets the word from the file
-        return "";
+
+        return tempWord;
     }
+
 }
